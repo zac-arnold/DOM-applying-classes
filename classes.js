1affd81beef5d11423431d99803f6399dc9af2d5
Zac Arnold <videozac@gmail.com>
2020-04-30 17:08:12 +1200

    create colored cirles 2 and 3

--- classes.js
+++ classes.js
@@ -5,9 +5,12 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
+  one()
     
   // Your turn! Create a new function called `two`, then call it from here.
+  two()
+  three ()
+  makeVisible()
 }
 
 function one () {
@@ -19,7 +22,19 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
-
+function two(){
+var two = document.getElementById('two')
+two.classList.add('green')
+}
 // CREATE FUNCTION three HERE
-
+function three(){
+  var three = document.getElementById('three')
+  three.classList.add('purple')
+}
 // CREATE FUNCTION makeVisible HERE
+function makeVisible(){
+  var makeVisible = document.getElementsByClassName('invisible')
+  console.log(getElementsByClassName)
+  invisible.classList.add('visible')
+
+}
